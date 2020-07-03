7cd94a011008b1a31a8b9d4e47176ecf10de436b
angelicaadan <angelicaadan06@gmail.com>
2020-07-03 18:08:57 -0400

    post y put ticket

--- ApiTicketingTool/ApiTicketingTool/Models/TicketFreshdesk.cs
+++ ApiTicketingTool/ApiTicketingTool/Models/TicketFreshdesk.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace ApiTicketingTool.Models
 {
+    //----Clase para GET de tickets
     public class TicketFreshDesk
     {
 
@@ -27,6 +28,8 @@ namespace ApiTicketingTool.Models
         public Json custom_fields { get; set; }
         public string tags { get; set; }
     }
+    //----Clases para POST de tickets
+
     public class PostTicketFreshDesk
     {
         public int? email_config_id { get; set; }
@@ -35,7 +38,7 @@ namespace ApiTicketingTool.Models
         public Int64 requester_id { get; set; }
         public Int64 responder_id { get; set; }
         public int source { get; set; }
-        public int status { get; set; }//status
+        public int status { get; set; }
         public string subject { get; set; }
         public int? product_id { get; set; }
         public string type { get; set; }
@@ -47,49 +50,51 @@ namespace ApiTicketingTool.Models
     {
         public string cliente { get; set; }
         public string id_cotizador { get; set; }
-        public Int64? proyecto { get; set; }
+        public int? proyecto { get; set; }
         public string diseo { get; set; }
-        public Double? horas_estimadas_por_cliente { get; set; }
-        public DateTime? cf_fecha_de_estimada_inicio { get; set; }
+        public double? horas_estimadas_por_cliente { get; set; }
+        public string cf_fecha_de_estimada_inicio { get; set; }
         public int? porcentaje_de_avance { get; set; }
-        public Double? horas_tampm_semana_2 { get; set; }
-        public Double? horas_tampm_semana_1 { get; set; }
+        public double? horas_tampm_semana_2 { get; set; }
+        public double? horas_tampm_semana_1 { get; set; }
         public string tickets_relacionados { get; set; }
         public string cf_fecha_de_estimada_entrega { get; set; }
-        public Double? horas_tampm_semana_3 { get; set; }
-        public DateTime? cf_fecha_de_real_inicio { get; set; }
-        public Double? horas_tampm_semana_4 { get; set; }
-        public DateTime? cf_fecha_de_real_entrega { get; set; }
-        public Double? avance_semana_2 { get; set; }
-        public Double? avance_semana_3 { get; set; }
-        public Double? avance_semana_4 { get; set; }
-        public Double? horas_garanta { get; set; }
-        public Int64 cf_horas_estimadas_por_agente { get; set; }
+        public double? horas_tampm_semana_3 { get; set; }
+        public string cf_fecha_de_real_inicio { get; set; }
+        public double? horas_tampm_semana_4 { get; set; }
+        public string cf_fecha_de_real_entrega { get; set; }
+        public double? avance_semana_2 { get; set; }
+        public double? avance_semana_3 { get; set; }
+        public double? avance_semana_4 { get; set; }
+        public double? horas_garanta { get; set; }
+        public Int64? cf_horas_estimadas_por_agente { get; set; }
         public string mes_facturacin { get; set; }
         public string se_incluyeron_pruebas_unitarias { get; set; }
         public string se_incluyeron_objetos_de_seguridad { get; set; }
 
     }
+    //----Clases para PUT de tickets
+
     public class JsonPut
     {
         public List<string> tickets_relacionados { get; set; }
 
     }
-    public class PutTicketFreshDesk
+    public class PutTicket
     {
         public JsonPut custom_fields { get; set; }
     }
-    public class JsonPut2
+    public class JsonPutstring
     {
         public string tickets_relacionados { get; set; }
 
     }
-    public class Put2TicketFreshDesk
+    public class PutStringTicket
     {
-        public Put2TicketFreshDesk()
+        public PutStringTicket()
         {
-            this.custom_fields = new JsonPut2();
+            this.custom_fields = new JsonPutstring();
         }
-        public JsonPut2 custom_fields { get; set; }
+        public JsonPutstring custom_fields { get; set; }
     }
 }
